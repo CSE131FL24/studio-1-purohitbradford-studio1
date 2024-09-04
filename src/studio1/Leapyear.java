e1868596fa248e69cb561138a382debd72f79c32
paulbradford <paulbradford@localhost>
2024-09-04 14:17:31 -0500

    studio 1 NOT COMPLETE

---
+++ src/studio1/Leapyear.java
@@ -0,0 +1,18 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("year?");
+		int year = in.nextInt();
+		boolean is0rdered = year/4 && ;
+		System.out.println(is0rdered);
+		
+
+	}
+
+}
